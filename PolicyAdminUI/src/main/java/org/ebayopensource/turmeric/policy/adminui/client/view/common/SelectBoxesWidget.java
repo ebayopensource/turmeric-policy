caf27917546ab2496cbf66a009223821f619747d
mchinea <mchinea@f631a2f7-0bfe-4ef6-91ca-5e61167123dd>
2011-05-12 15:33:11 +0000

    TURMERIC-1253: Added paging tables to all the list-related components in the resources/subject/subjectgroups assignment views.
    git-svn-id: http://www.ebayopensource.org/svn/turmeric/soa-platform/trunk/policy@991 f631a2f7-0bfe-4ef6-91ca-5e61167123dd

--- PolicyAdminUI/src/main/java/org/ebayopensource/turmeric/policy/adminui/client/view/common/SelectBoxesWidget.java
+++ PolicyAdminUI/src/main/java/org/ebayopensource/turmeric/policy/adminui/client/view/common/SelectBoxesWidget.java
@@ -12,9 +12,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.ebayopensource.turmeric.policy.adminui.client.PolicyAdminUIUtil;
+import org.ebayopensource.turmeric.policy.adminui.client.model.policy.PolicySubjectAssignment;
+
+import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.cell.client.ClickableTextCell;
+import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.cell.client.Cell.Context;
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.Header;
+import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
@@ -24,144 +40,301 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.MultiSelectionModel;
+import com.google.gwt.view.client.ProvidesKey;
+import com.google.gwt.view.client.SelectionChangeEvent;
 
+// TODO: Auto-generated Javadoc
 /**
- * SelectBoxesWidget
- *
+ * SelectBoxesWidget.
  */
 public class SelectBoxesWidget extends Composite {
-    private Panel panel;
-    private ListBox availableBox;
-    private ListBox selectedBox;
-    protected Grid grid;
-    protected Button addButton;
-    protected Button delButton;
-    protected Label availableLabel;
-    protected Label selectedLabel;
-    
-    
-    public SelectBoxesWidget (String availableName, boolean isAvailableMulti, String selectedName, boolean isSelectedMulti) {
-        panel = new SimplePanel();
-        availableLabel = new Label(availableName);
-        selectedLabel = new Label(selectedName);
-        
-        availableBox = new ListBox(isAvailableMulti);
-        selectedBox = new ListBox(isSelectedMulti);
-        availableBox.setWidth("200px");
-        availableBox.setHeight("80px");
-        selectedBox.setWidth("200px");
-        selectedBox.setHeight("80px");
-        
-        //arrows
-        addButton = new Button(">>");
-        delButton = new Button("<<");
-        
-        addButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                move(availableBox, selectedBox);
-            }  
-        });
-        
-        delButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-               move(selectedBox, availableBox);
-            }  
-        });
-        
-        grid = new Grid(2,3);
-        grid.setWidget(0, 0, availableLabel);
-        grid.setWidget(0, 2, selectedLabel);
-        grid.setWidget(1, 0, availableBox);
-        grid.getCellFormatter().setHorizontalAlignment(1,0, HasHorizontalAlignment.ALIGN_CENTER);
-        
-        Grid arrowGrid = new Grid(2,1);
-        arrowGrid.setWidget(0,0,addButton);
-        arrowGrid.setWidget(1,0,delButton);
-        arrowGrid.setWidth("80px");
-        arrowGrid.getCellFormatter().setHorizontalAlignment(0,0, HasHorizontalAlignment.ALIGN_CENTER);
-        arrowGrid.getCellFormatter().setHorizontalAlignment(1,0, HasHorizontalAlignment.ALIGN_CENTER);
-        grid.setWidget(1, 1, arrowGrid);
-        grid.getCellFormatter().setVerticalAlignment(1, 1, HasVerticalAlignment.ALIGN_MIDDLE);
-        grid.getCellFormatter().setHorizontalAlignment(1,1, HasHorizontalAlignment.ALIGN_CENTER);
-        
-        grid.setWidget(1, 2, selectedBox);
-        grid.getCellFormatter().setHorizontalAlignment(1,2, HasHorizontalAlignment.ALIGN_CENTER);
-        panel.add(grid);
-        initWidget(panel);
-    }
-    
+	
+	/** The avail table. */
+	private CellTable<String> availTable;
+	
+	/** The avail key provider. */
+	private ProvidesKey<String> availKeyProvider;
+	
+	/** The avail selection model. */
+	private MultiSelectionModel<String> availSelectionModel;
+	
+	/** The avail selections. */
+	private List<String> availSelections = new ArrayList<String>();
+	
+	/** The avail data provider. */
+	private ListDataProvider<String> availDataProvider;
+	
+	/** The selected table. */
+	private CellTable<String> selectedTable;
+	
+	/** The selected key provider. */
+	private ProvidesKey<String> selectedKeyProvider;
+	
+	/** The selected selection model. */
+	private MultiSelectionModel<String> selectedSelectionModel;
+	
+	/** The selected selections. */
+	private List<String> selectedSelections = new ArrayList<String>();
+	
+	/** The selected data provider. */
+	private ListDataProvider<String> selectedDataProvider;
+	
+	/** The avail pager. */
+	private SimplePager availPager;
+	
+	/** The selected pager. */
+	private SimplePager selectedPager;
+	
+	/** The panel. */
+	private Panel panel;
+	
+	/** The grid. */
+	protected Grid grid;
+	
+	/** The add button. */
+	protected Button addButton;
+	
+	/** The del button. */
+	protected Button delButton;
+	
+	/** The available label. */
+	protected Label availableLabel;
+	
+	/** The selected label. */
+	protected Label selectedLabel;
 
-    public List<String> getSelections () {
-        List<String> selectedSubjects = new ArrayList<String>();
-        for (int i=0; i<selectedBox.getItemCount();i++)
-            selectedSubjects.add(selectedBox.getItemText(i));
+	/**
+	 * Instantiates a new select boxes widget.
+	 *
+	 * @param availableName the available name
+	 * @param isAvailableMulti the is available multi
+	 * @param selectedName the selected name
+	 * @param isSelectedMulti the is selected multi
+	 */
+	public SelectBoxesWidget(String availableName, boolean isAvailableMulti,
+			String selectedName, boolean isSelectedMulti) {
+		configureAvailableTable();
+		configureSelectedTable();
 
-        return selectedSubjects;
-    }
-    
-    public List<String> getAvailables () {
-        List<String> availables = new ArrayList<String>();
-        for (int i=0; i<availableBox.getItemCount();i++)
-            availables.add(availableBox.getItemText(i));
+		panel = new SimplePanel();
+		availableLabel = new Label(availableName);
+		selectedLabel = new Label(selectedName);
 
-        return availables;
-    }
-    
-    
-    public void setAvailables (List<String> availables) {
-        availableBox.clear();
-        if (availables != null && availables.size() >0) {
-            for (String s:availables) {
-                if (! contains(selectedBox, s))
-                    availableBox.addItem(s);
-            }
-        }
-    }
-    
-    public void setWithForOperations(){
-    	availableBox.setWidth("230px");
-    	selectedBox.setWidth("230px");
-    }
-    
-    public void setSelections (List<String> selects) {
-        selectedBox.clear();
-        if (selects != null && selects.size() > 0)
-            for (String s:selects) {
-                if (! contains(availableBox, s))
-                    selectedBox.addItem(s);
-            }
-    }
-    
-    protected boolean contains (ListBox box, String string) {
-        if (box == null)
-            return false;
-        if (string == null)
-            return false;
-        
-        boolean result = false;
-        for (int i=0; (i< box.getItemCount()) && !result; i++) {
-            result = string.equals(box.getItemText(i));
-        }
-        return result;
-    }
-    
-    protected void move (ListBox from, ListBox to) {
-        int count = from.getItemCount();
-        Map<Integer, String> selections = new HashMap<Integer, String>();
-        
-        for (int i=0;i<count;i++)
-            if (from.isItemSelected(i))
-                selections.put(new Integer(i), from.getItemText(i));
-        
-        for (Map.Entry<Integer, String> entry:selections.entrySet()) {
-            to.addItem(entry.getValue());
-            boolean removed = false;
-            for (int i=0;i<count && !removed;i++) {
-                if (entry.getValue().equals(from.getItemText(i))) {
-                    from.removeItem(i);
-                    removed = true;
-                }
-            }
-        } 
-    }
+
+		// arrows
+		addButton = new Button(">>");
+		delButton = new Button("<<");
+
+		addButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				moveToSelected();
+			}
+		});
+
+		delButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				moveToAvailable();
+			}
+		});
+
+		grid = new Grid(3, 3);
+		grid.setWidget(0, 0, availableLabel);
+		grid.setWidget(0, 2, selectedLabel);
+		grid.setWidget(1, 0, availTable);
+		grid.setWidget(2, 0, availPager);
+		grid.setWidget(2, 2, selectedPager);
+		grid.getCellFormatter().setHorizontalAlignment(1, 0,
+				HasHorizontalAlignment.ALIGN_CENTER);
+
+		Grid arrowGrid = new Grid(2, 1);
+		arrowGrid.setWidget(0, 0, addButton);
+		arrowGrid.setWidget(1, 0, delButton);
+		arrowGrid.setWidth("80px");
+		arrowGrid.getCellFormatter().setHorizontalAlignment(0, 0,
+				HasHorizontalAlignment.ALIGN_CENTER);
+		arrowGrid.getCellFormatter().setHorizontalAlignment(1, 0,
+				HasHorizontalAlignment.ALIGN_CENTER);
+		grid.setWidget(1, 1, arrowGrid);
+		grid.getCellFormatter().setVerticalAlignment(1, 1,
+				HasVerticalAlignment.ALIGN_MIDDLE);
+		grid.getCellFormatter().setHorizontalAlignment(1, 1,
+				HasHorizontalAlignment.ALIGN_CENTER);
+
+		grid.setWidget(1, 2, selectedTable);
+		grid.getCellFormatter().setHorizontalAlignment(1, 2,
+				HasHorizontalAlignment.ALIGN_CENTER);
+		panel.add(grid);
+		initWidget(panel);
+	}
+
+	
+	/**
+	 * Configure available table.
+	 */
+	private void configureAvailableTable() {
+		availTable = new CellTable<String>(availKeyProvider);
+		availSelectionModel = new MultiSelectionModel<String>(availKeyProvider);
+		availTable.setSelectionModel(availSelectionModel);
+		availSelectionModel
+				.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+					public void onSelectionChange(SelectionChangeEvent event) {
+						Set<String> x = availSelectionModel.getSelectedSet();
+						availSelections.clear();
+						if (x != null)
+							availSelections.addAll(x);
+					}
+				});
+		availDataProvider = new ListDataProvider<String>();
+		availDataProvider.addDataDisplay(availTable);
+
+		availPager = new TurmericPager();
+		availPager.setPageSize(5);
+		availPager.setDisplay(availTable);
+
+		// text column for type
+		ClickableTextCell sbTypeCellClickable = new ClickableTextCell();
+		Column<String, String> typeCol = new Column<String, String>(
+				sbTypeCellClickable) {
+
+			@Override
+			public String getValue(String arg0) {
+				// TODO Auto-generated method stub
+				return arg0;
+			}
+		};
+		availTable.addColumn(typeCol,
+				"");
+	}
+	
+	/**
+	 * Configure selected table.
+	 */
+	private void configureSelectedTable() {
+		selectedTable = new CellTable<String>(selectedKeyProvider);
+		selectedSelectionModel = new MultiSelectionModel<String>(selectedKeyProvider);
+		selectedTable.setSelectionModel(selectedSelectionModel);
+		selectedSelectionModel
+				.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+					public void onSelectionChange(SelectionChangeEvent event) {
+						Set<String> x = selectedSelectionModel.getSelectedSet();
+						selectedSelections.clear();
+						if (x != null)
+							selectedSelections.addAll(x);
+					}
+				});
+		selectedDataProvider = new ListDataProvider<String>();
+		selectedDataProvider.addDataDisplay(selectedTable);
+
+		selectedPager = new TurmericPager();
+		selectedPager.setPageSize(5);
+		selectedPager.setDisplay(selectedTable);
+
+		// text column for type
+		ClickableTextCell sbTypeCellClickable = new ClickableTextCell();
+		Column<String, String> typeCol = new Column<String, String>(
+				sbTypeCellClickable) {
+
+			@Override
+			public String getValue(String arg0) {
+				// TODO Auto-generated method stub
+				return arg0;
+			}
+		};
+		selectedTable.addColumn(typeCol,
+				"");
+	}
+
+	/**
+	 * Gets the selections.
+	 *
+	 * @return the selections
+	 */
+	public List<String> getSelections() {
+		return selectedDataProvider.getList();
+	}
+
+	/**
+	 * Gets the availables.
+	 *
+	 * @return the availables
+	 */
+	public List<String> getAvailables() {
+		return availDataProvider.getList();
+	}
+
+	/**
+	 * Sets the availables.
+	 *
+	 * @param availables the new availables
+	 */
+	public void setAvailables(List<String> availables) {
+		availDataProvider.getList().clear();
+		availPager.setPageSize(5);
+		availPager.setRangeLimited(true);
+		if (availables != null && availables.size() > 0) {
+			for (String s : availables) {
+				if (!selectedDataProvider.getList().contains(s))
+					availDataProvider.getList().add(s);
+			}
+		}
+		availTable.redraw();
+	}
+
+	/**
+	 * Sets the with for operations.
+	 */
+	public void setWithForOperations() {
+
+	}
+
+	/**
+	 * Sets the selections.
+	 *
+	 * @param selects the new selections
+	 */
+	public void setSelections(List<String> selects) {
+		selectedDataProvider.getList().clear();
+		selectedPager.setPageSize(5);
+		selectedPager.setRangeLimited(true);
+		if (selects != null && selects.size() > 0) {
+			for (String s : selects) {
+				if (!availDataProvider.getList().contains(s))
+					selectedDataProvider.getList().add(s);
+			}
+		}
+		selectedTable.redraw();
+	}
+
+	/**
+	 * Move to available.
+	 */
+	protected void moveToAvailable() {
+		if (!selectedSelections.isEmpty()) {
+			List<String> removedElements = new ArrayList<String>();
+			for (String selectedElemnt : selectedSelections) {
+				availDataProvider.getList().add(selectedElemnt);
+				removedElements.add(selectedElemnt);
+			}
+			selectedDataProvider.getList().removeAll(removedElements);
+			selectedSelections.clear();
+		}
+	}
+
+	/**
+	 * Move to selected.
+	 */
+	protected void moveToSelected() {
+		if (!availSelections.isEmpty()) {
+			List<String> removedElements = new ArrayList<String>();
+			for (String selectedElemnt : availSelections) {
+				selectedDataProvider.getList().add(selectedElemnt);
+				removedElements.add(selectedElemnt);
+			}
+			availDataProvider.getList().removeAll(removedElements);
+			availSelections.clear();
+		}
+	}
+
 }
