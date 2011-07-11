fbf97cca5bea741d158c5982837f9836ebfcbe6a
josealvarezmuguerza <muguerza@intalio.com>
2011-07-11 12:01:37 -0300

    TURMERIC-1393 - fixing UT

---
+++ PolicyProviderImpl/src/main/java/org/ebayopensource/turmeric/policyservice/provider/CalculatedGroupMembershipProviderImpl.java
@@ -0,0 +1,25 @@
+package org.ebayopensource.turmeric.policyservice.provider;
+
+import java.util.Collection;
+
+import org.ebayopensource.turmeric.policyservice.exceptions.PolicyProviderException;
+import org.ebayopensource.turmeric.security.v1.services.SubjectGroupType;
+
+public class CalculatedGroupMembershipProviderImpl implements
+		CalculatedGroupMembershipProvider {
+
+	@Override
+	public SubjectGroupType getCalculatedSG(SubjectGroupType subjectGroup)
+			throws PolicyProviderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<SubjectGroupType> getAllCalculatedSGs()
+			throws PolicyProviderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
